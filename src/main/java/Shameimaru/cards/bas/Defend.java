c20b2c5269f887362b29d28f44e59640c97d3b20
Bailey <erringtonbusiness@gmail.com>
2021-01-27 12:20:10 +0000

    Update Defend for new project

--- src/main/java/Shameimaru/cards/bas/Defend.java
+++ src/main/java/Shameimaru/cards/bas/Defend.java
@@ -1,17 +1,17 @@
 package Shameimaru.cards.bas;
 
 
-import Limitbreaker.cards.abs.abs_lb_card;
-import Limitbreaker.util.CardInfo;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-import static Limitbreaker.Limitbreaker.makeID;
-import static Limitbreaker.util.actionShortcuts.doDef;
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.doDef;
 
 
-public class Defend extends abs_lb_card {
+public class Defend extends abs_aya_card {
     private final static CardInfo cardInfo = new CardInfo(
             Defend.class.getSimpleName(),
             COSTS[1],
