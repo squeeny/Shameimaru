b1331114a1f809f9abcaa78247977afedad52aa1
Bailey <erringtonbusiness@gmail.com>
2021-01-28 09:31:57 +0000

    [Propaganda] Correct internal name

--- src/main/java/Shameimaru/powers/WidespreadPropagandaPower.java
+++ src/main/java/Shameimaru/powers/WidespreadPropagandaPower.java
@@ -11,7 +11,7 @@ import static Shameimaru.Shameimaru.makeID;
 
 public class WidespreadPropagandaPower extends AbstractPower
 {
-    public static final String POWER_ID = makeID(WidespreadPropaganda.class.getSimpleName());
+    public static final String POWER_ID = makeID(WidespreadPropagandaPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
