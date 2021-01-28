6c8e664431e060e8c6da0ecb8161fea82b2674dd
Bailey <erringtonbusiness@gmail.com>
2021-01-28 23:17:44 +0000

    New Card - Doctored Image

---
+++ src/main/java/Shameimaru/actions/photograph/upgradePhotographAction.java
@@ -0,0 +1,52 @@
+package Shameimaru.actions.photograph;
+
+import Shameimaru.cards.sp.Photograph;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+import java.util.ArrayList;
+
+import static Shameimaru.util.actionShortcuts.att;
+import static Shameimaru.util.actionShortcuts.p;
+
+public class upgradePhotographAction extends AbstractGameAction {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("AnyCardFromDeckToHandAction");
+    public static final String[] TEXT = uiStrings.TEXT;
+    private int upgTimes;
+    public upgradePhotographAction(int amount, int timesToUpgrade) {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.duration = Settings.ACTION_DUR_FAST;
+        this.amount = amount;
+        upgTimes = timesToUpgrade;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            CardGroup tmp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+            for(AbstractCard c: p().hand.group) {
+                if (c instanceof Photograph) { tmp.addToTop(c); }
+            }
+            if (tmp.size() < amount) {
+                this.isDone = true;
+                return;
+            }
+            AbstractDungeon.gridSelectScreen.open(tmp, this.amount, TEXT[1], false);
+            tickDuration();
+        }
+
+        else if (AbstractDungeon.gridSelectScreen.selectedCards.size() != 0) {
+            for (AbstractCard card : AbstractDungeon.gridSelectScreen.selectedCards) {
+                for(int i = 0; i < upgTimes; i+= 1){ card.upgrade(); }
+            }
+            AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
+            this.isDone = true;
+        }
+    }
+}
---
+++ src/main/java/Shameimaru/cards/com/DoctoredImage.java
@@ -0,0 +1,37 @@
+package Shameimaru.cards.com;
+
+import Shameimaru.actions.photograph.upgradePhotographAction;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.atb;
+import static Shameimaru.util.actionShortcuts.doDef;
+
+public class DoctoredImage extends abs_aya_card {
+    private final static CardInfo cardInfo = new CardInfo(
+            DoctoredImage.class.getSimpleName(),
+            COSTS[1],
+            AbstractCard.CardType.SKILL,
+            AbstractCard.CardTarget.SELF
+    );
+    public static final String ID = makeID(cardInfo.cardName);
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 4;
+    private static final int UPGRADE_AMOUNT = 1;
+    private static final int UPG_UPGRADE_AMOUNT = 1;
+
+    public DoctoredImage() {
+        super(cardInfo, true);
+        setBlock(BLOCK, UPG_BLOCK);
+        setMagic(UPGRADE_AMOUNT, UPG_UPGRADE_AMOUNT);
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        doDef(this.block);
+        atb(new upgradePhotographAction(1, magicNumber));
+    }
+}
