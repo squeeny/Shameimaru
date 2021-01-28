8f214cc947736fd8b453dcb28c4b70a9d24b9715
Bailey <erringtonbusiness@gmail.com>
2021-01-28 08:44:16 +0000

    Implementation - Widespread Propaganda

--- src/main/java/Shameimaru/cards/sp/core/attackCard.java
+++ src/main/java/Shameimaru/cards/sp/core/attackCard.java
@@ -1,14 +1,17 @@
 package Shameimaru.cards.sp.core;
 
 import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.cards.unc.WidespreadPropaganda;
+import Shameimaru.powers.WidespreadPropagandaPower;
 import Shameimaru.util.CardInfo;
 import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static Shameimaru.Shameimaru.makeID;
-import static Shameimaru.util.actionShortcuts.doDmg;
-import static Shameimaru.util.actionShortcuts.doPow;
+import static Shameimaru.util.actionShortcuts.*;
 
 @AutoAdd.Ignore
 public class attackCard extends abs_aya_card {
@@ -27,6 +30,10 @@ public class attackCard extends abs_aya_card {
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for(int i = 0; i <= magicNumber; i+=1){ doDmg(m, damage); }
+        boolean hasWideSpread = p.hasPower(WidespreadPropagandaPower.POWER_ID);
+        for(int i = 0; i <= magicNumber; i+=1){
+            if(hasWideSpread){ doAllDmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL, false); }
+            else { doDmg(m, damage); }
+        }
     }
 }
---
+++ src/main/java/Shameimaru/powers/WidespreadPropagandaPower.java
@@ -0,0 +1,32 @@
+package Shameimaru.powers;
+
+import Shameimaru.cards.unc.WidespreadPropaganda;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static Shameimaru.Shameimaru.makeID;
+
+
+public class WidespreadPropagandaPower extends AbstractPower
+{
+    public static final String POWER_ID = makeID(WidespreadPropaganda.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public WidespreadPropagandaPower(AbstractCreature owner) { this(owner, -1); }
+    public WidespreadPropagandaPower(AbstractCreature owner, int amount)
+    {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+        type = PowerType.BUFF;
+        updateDescription();
+        loadRegion("flameBarrier");
+        canGoNegative = false;
+    }
+    @Override
+    public void updateDescription() { this.description = DESCRIPTIONS[0]; }
+}
