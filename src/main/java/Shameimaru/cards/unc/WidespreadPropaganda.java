43ef3d21a1bd6231a754bf5f0f793212aa2c81a9
Bailey <erringtonbusiness@gmail.com>
2021-01-28 08:39:35 +0000

    Card Framework - WidespreadPropaganda
    2 Cost Power [Uncommon]
    
    Whenever a Newspaper would deal damage to a single target, it deals damage to all enemies instead.
    
    Upgrades to 1 cost.

---
+++ src/main/java/Shameimaru/cards/unc/WidespreadPropaganda.java
@@ -0,0 +1,33 @@
+package Shameimaru.cards.unc;
+
+import Shameimaru.actions.photograph.createPhotographAction;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.atb;
+import static Shameimaru.util.actionShortcuts.doDmg;
+
+public class WidespreadPropaganda extends abs_aya_card {
+    private final static CardInfo cardInfo = new CardInfo(
+            WidespreadPropaganda.class.getSimpleName(),
+            COSTS[2],
+            CardType.POWER,
+            CardTarget.SELF
+    );
+    public static final String ID = makeID(cardInfo.cardName);
+    private static final int AMOUNT = -1;
+    private static final int UPG_COST = 1;
+    public WidespreadPropaganda() {
+        super(cardInfo, false);
+        setCostUpgrade(UPG_COST);
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+    }
+}
