83d1b1c52bffef55a2cae23efeac2d5c670d4fa6
Bailey <erringtonbusiness@gmail.com>
2021-01-28 21:14:18 +0000

    New card - Photocopier
    Photocopier - Uncommon - 1 Cost
    Gain 7 (11) Block.
    Add a copy of a photograph to your hand.

---
+++ src/main/java/Shameimaru/cards/unc/Photocopier.java
@@ -0,0 +1,36 @@
+package Shameimaru.cards.unc;
+
+import Shameimaru.actions.photograph.duplicatePhotographAction;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.atb;
+import static Shameimaru.util.actionShortcuts.doDef;
+
+
+public class Photocopier extends abs_aya_card {
+    private final static CardInfo cardInfo = new CardInfo(
+            Photocopier.class.getSimpleName(),
+            COSTS[1],
+            AbstractCard.CardType.SKILL,
+            AbstractCard.CardTarget.SELF
+    );
+    public static final String ID = makeID(cardInfo.cardName);
+    private static final int BLOCK = 7;
+    private static final int UPG_BLOCK = 4;
+    private static final int COPIES_AMOUNT = 1;
+    public Photocopier() {
+        super(cardInfo, false);
+        setBlock(BLOCK, UPG_BLOCK);
+        setMagic(COPIES_AMOUNT);
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        doDef(this.block);
+        atb(new duplicatePhotographAction(magicNumber));
+    }
+}
