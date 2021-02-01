16de49828a36f3a2f557139978a2427c44cf9cf0
Bailey <erringtonbusiness@gmail.com>
2021-02-01 18:16:48 +0000

    New card -> Photoshoot.

---
+++ src/main/java/Shameimaru/cards/com/Photoshoot.java
@@ -0,0 +1,32 @@
+package Shameimaru.cards.com;
+
+import Shameimaru.actions.photograph.createPhotographAction;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.*;
+
+public class Photoshoot extends abs_aya_card {
+    private final static CardInfo cardInfo = new CardInfo(
+            Photoshoot.class.getSimpleName(),
+            COSTS[1],
+            CardType.SKILL,
+            AbstractCard.CardTarget.ENEMY
+    );
+    public static final String ID = makeID(cardInfo.cardName);
+    private static final int BLOCK = 8;
+    private static final int UPG_BLOCK = 12;
+    public Photoshoot() {
+        super(cardInfo, true);
+        setBlock(BLOCK, UPG_BLOCK);
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        doDef(block);
+        atb(new createPhotographAction(m, this.upgraded));
+    }
+}
