780f0aa4e85e05b6bc06c52511018cb4c5b07198
Bailey <erringtonbusiness@gmail.com>
2021-02-02 14:26:57 +0000

    New card - CandidShot

---
+++ src/main/java/Shameimaru/cards/com/CandidShot.java
@@ -0,0 +1,33 @@
+package Shameimaru.cards.com;
+
+import Shameimaru.actions.photograph.createPhotographAction;
+import Shameimaru.cards.abs.abs_aya_card;
+import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Shameimaru.Shameimaru.makeID;
+import static Shameimaru.util.actionShortcuts.atb;
+import static Shameimaru.util.actionShortcuts.doDmg;
+
+public class CandidShot extends abs_aya_card {
+    private final static CardInfo cardInfo = new CardInfo(
+            CandidShot.class.getSimpleName(),
+            COSTS[1],
+            AbstractCard.CardType.ATTACK,
+            AbstractCard.CardTarget.ENEMY
+    );
+    public static final String ID = makeID(cardInfo.cardName);
+    private static final int DMG = 8;
+    private static final int UPG_DMG = 2;
+    public CandidShot() {
+        super(cardInfo, false);
+        setDamage(DMG, UPG_DMG);
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        doDmg(m, this.damage);
+        atb(new createPhotographAction(m, this.upgraded));
+    }
+}
