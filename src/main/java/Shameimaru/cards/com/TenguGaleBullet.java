ba2b4187f5aa0e58577398740677e593e29b0304
Bailey <erringtonbusiness@gmail.com>
2021-02-02 14:30:58 +0000

    New card: Tengu Gale Bullet

--- src/main/java/Shameimaru/cards/com/TenguGaleBullet.java
+++ src/main/java/Shameimaru/cards/com/TenguGaleBullet.java
@@ -3,31 +3,29 @@ package Shameimaru.cards.com;
 import Shameimaru.actions.photograph.createPhotographAction;
 import Shameimaru.cards.abs.abs_aya_card;
 import Shameimaru.util.CardInfo;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Shameimaru.Shameimaru.makeID;
-import static Shameimaru.util.actionShortcuts.atb;
-import static Shameimaru.util.actionShortcuts.doDmg;
+import static Shameimaru.util.actionShortcuts.*;
 
-public class CandidShot extends abs_aya_card {
+public class TenguGaleBullet extends abs_aya_card {
     private final static CardInfo cardInfo = new CardInfo(
-            CandidShot.class.getSimpleName(),
+            TenguGaleBullet.class.getSimpleName(),
             COSTS[1],
             AbstractCard.CardType.ATTACK,
             AbstractCard.CardTarget.ENEMY
     );
     public static final String ID = makeID(cardInfo.cardName);
     private static final int DMG = 8;
-    private static final int UPG_DMG = 2;
-    public CandidShot() {
-        super(cardInfo, true);
+    private static final int UPG_DMG = 3;
+    public TenguGaleBullet() {
+        super(cardInfo, false);
         setDamage(DMG, UPG_DMG);
     }
     @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {
-        doDmg(m, this.damage);
-        atb(new createPhotographAction(m, this.upgraded));
-    }
+    public void use(AbstractPlayer p, AbstractMonster m) { doAllDmg(this.damage, AbstractGameAction.AttackEffect.NONE, false); }
+    public void triggerOnManualDiscard(){ doAllDmg(this.damage, AbstractGameAction.AttackEffect.NONE, false); }
 }
