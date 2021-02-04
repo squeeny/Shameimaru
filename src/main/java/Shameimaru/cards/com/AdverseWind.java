57dc82925de04010f99f076e2e03eed6bbd19061
Bailey <erringtonbusiness@gmail.com>
2021-02-04 05:43:26 +0000

    New Card -> AdverseWind

--- src/main/java/Shameimaru/cards/com/AdverseWind.java
+++ src/main/java/Shameimaru/cards/com/AdverseWind.java
@@ -3,7 +3,6 @@ package Shameimaru.cards.com;
 import Shameimaru.actions.unique.airSlash.airSlashAction;
 import Shameimaru.cards.abs.abs_aya_card;
 import Shameimaru.util.CardInfo;
-import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -11,27 +10,27 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import static Shameimaru.Shameimaru.makeID;
 import static Shameimaru.util.actionShortcuts.*;
 
-public class AirSlash extends abs_aya_card {
+public class AdverseWind extends abs_aya_card {
     private final static CardInfo cardInfo = new CardInfo(
-            AirSlash.class.getSimpleName(),
+            AdverseWind.class.getSimpleName(),
             COSTS[2],
-            AbstractCard.CardType.ATTACK,
-            AbstractCard.CardTarget.ENEMY
+            CardType.SKILL,
+            CardTarget.SELF
     );
     public static final String ID = makeID(cardInfo.cardName);
-    private static final int DMG = 12;
-    private static final int UPG_DMG = 4;
+    private static final int BLOCK = 12;
+    private static final int UPG_BLOCK = 4;
     private static final int DRAW = 1;
     private static final int REFUND_GOOD = 2;
     private static final int REFUND_BAD = 1;
-    public AirSlash() {
+    public AdverseWind() {
         super(cardInfo, false);
-        setDamage(DMG, UPG_DMG);
+        setBlock(BLOCK, UPG_BLOCK);
         setMagic(DRAW);
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        doDmg(m, damage);
+        doDef(block);
         doDraw(magicNumber);
         atb(new airSlashAction(REFUND_GOOD, REFUND_BAD));
     }
