54d5522e39631f12ef5bfd950e4b1e9272adf7f4
Bailey <erringtonbusiness@gmail.com>
2021-02-05 01:34:46 +0000

    Incorrect UPG_BLOCK [Playful Taunt]

--- src/main/java/Shameimaru/cards/bas/PlayfulTaunt.java
+++ src/main/java/Shameimaru/cards/bas/PlayfulTaunt.java
@@ -21,7 +21,7 @@ public class PlayfulTaunt extends abs_aya_card {
     );
     public static final String ID = makeID(cardInfo.cardName);
     private static final int BLOCK = 8;
-    private static final int UPG_BLOCK = 12;
+    private static final int UPG_BLOCK = 3;
     public PlayfulTaunt() {
         super(cardInfo, false);
         setBlock(BLOCK, UPG_BLOCK);
