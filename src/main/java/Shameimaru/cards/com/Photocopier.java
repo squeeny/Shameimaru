64c92ebb4a24177f93b813da7fbc0fcf1df32243
Bailey <erringtonbusiness@gmail.com>
2021-02-09 03:57:34 +0000

    Nerf Photocopier
    Photocopier -> Removed additional effect.
    Photocopier -> Removed ability to target itself
    Photocopier -> Added fleeting
    Photocopier -> Set base cost to 1
    Photocopier -> Set upgraded cost to 0.

--- src/main/java/Shameimaru/cards/com/Photocopier.java
+++ src/main/java/Shameimaru/cards/com/Photocopier.java
@@ -4,10 +4,12 @@ import Shameimaru.actions.DiscardAnyAction;
 import Shameimaru.cards.abs.abs_aya_card;
 import Shameimaru.util.CardInfo;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsAction;
+import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.FleetingField;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.unique.AddCardToDeckAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.UIStrings;
@@ -20,34 +22,28 @@ import static Shameimaru.util.actionShortcuts.p;
 public class Photocopier extends abs_aya_card {
     private final static CardInfo cardInfo = new CardInfo(
             Photocopier.class.getSimpleName(),
-            COSTS[0],
+            COSTS[1],
             CardType.SKILL,
             CardTarget.SELF
     );
     public static final String ID = makeID(cardInfo.cardName);
-    private static final int COPY = 1;
-    private static final int DRAW = 1;
-    private static final int ACTIVATE_PER_COPIES = 3;
-    private static final int UPG_ACTIVATE_PER_COPIES = -1;
+    private static final int COST = 0;
+    private static final int AMOUNT = 1;
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("RewardItem");
     public Photocopier() {
         super(cardInfo, false);
-        setMagic(DRAW);
-        setAyaMagic(ACTIVATE_PER_COPIES, UPG_ACTIVATE_PER_COPIES);
-        setExhaust(true);
+        setCostUpgrade(COST);
+        FleetingField.fleeting.set(this, true);
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        atb(new SelectCardsAction(p.masterDeck.group, COPY, uiStrings.TEXT[2], (cards) -> {
+        CardGroup eligible = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+        for(AbstractCard c: p.masterDeck.group){
+            if(!(c instanceof Photocopier)){ eligible.addToBottom(c); }
+        }
+        atb(new SelectCardsAction(eligible.group, AMOUNT, uiStrings.TEXT[2], (cards) -> {
             AbstractCard c = cards.get(0);
-            int countOfCard = 0;
-            for(AbstractCard cc: p.masterDeck.group){
-                if(cc.cardID.equals(c.cardID)){ countOfCard += 1; }
-            }
-            countOfCard += 1;
             att(new AddCardToDeckAction(c.makeCopy()));
-            doDraw(countOfCard / ayaSecondMagicNumber, true);
-            att(new GainEnergyAction(countOfCard / ayaSecondMagicNumber));
         }));
     }
 }
