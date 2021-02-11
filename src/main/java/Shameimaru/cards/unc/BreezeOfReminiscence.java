3d2754c5c8a12a4cfe9f7ebde8272957156f33d3
Bailey <erringtonbusiness@gmail.com>
2021-02-11 02:13:02 +0000

    BreezeOfReminiscence typo

--- src/main/java/Shameimaru/cards/unc/BreezeOfReminiscence.java
+++ src/main/java/Shameimaru/cards/unc/BreezeOfReminiscence.java
@@ -1,20 +1,17 @@
 package Shameimaru.cards.unc;
 
-import Shameimaru.actions.photograph.upgradePhotographAction;
 import Shameimaru.cards.abs.abs_aya_card;
 import Shameimaru.util.CardInfo;
 import com.megacrit.cardcrawl.actions.common.BetterDiscardPileToHandAction;
-import com.megacrit.cardcrawl.actions.utility.DiscardToHandAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Shameimaru.Shameimaru.makeID;
 import static Shameimaru.util.actionShortcuts.*;
 
-public class BreezeOfReminiscience extends abs_aya_card {
+public class BreezeOfReminiscence extends abs_aya_card {
     private final static CardInfo cardInfo = new CardInfo(
-            BreezeOfReminiscience.class.getSimpleName(),
+            BreezeOfReminiscence.class.getSimpleName(),
             COSTS[1],
             CardType.ATTACK,
             CardTarget.ENEMY
@@ -23,7 +20,7 @@ public class BreezeOfReminiscience extends abs_aya_card {
     private static final int DAMAGE = 10;
     private static final int UPG_DAMAGE = 4;
     private static final int AMOUNT = 1;
-    public BreezeOfReminiscience() {
+    public BreezeOfReminiscence() {
         super(cardInfo, false);
         setDamage(DAMAGE, UPG_DAMAGE);
         setMagic(AMOUNT);
